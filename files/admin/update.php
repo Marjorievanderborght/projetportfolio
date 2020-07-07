fdf083aa3ab1d4fbbad9babac3e7bd195d91307c
Marjorievanderborght <web2020.marjorie@gmail.com>
2020-07-07 16:57:28 +0200

    changement design page

--- files/admin/update.php
+++ files/admin/update.php
@@ -46,8 +46,8 @@ if(isset($_GET['id'])&&ctype_digit($_GET['id'])){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/css" href="jumbotron.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/jumbotron.css">
     <link href="#" rel="stylesheet" type="text/css">
 
 
